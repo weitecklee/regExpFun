3eedf0335b7fc548d05c97fb66477093853c8a25
arkteck <arkteck@gmail.com>
2022-06-12 22:15:08 -0400

    Presentation version

--- quiz.js
+++ quiz.js
@@ -1,20 +1,49 @@
-const secretCode = [
-  '1G1A11A3A13A1A6B6A1A8A',
-  '|B3B|@7@=(1)10@|@3\\1A=2@|@4@|',
-  '2@=@|B3B1A=|A|\\1C4B1A|@=1@|A1C@|@E=|',
-  "2@=@`A1/1\\1A'@B=\\1B=1\\1A'@|@1@1A\\1A1/|@|\\1\\1B=|",
-  '2@=@|@=(A|@|A2/B1/@(A|@=|A1@|=&@=2</B1/A|',
-  '2+A=+A=|A|/B,A|/B=|C\\2/B,A|A|A(1)+A=/C\\|A|A|/A/C&',
-  '41+\\',
-  '56G1A17A27B38A3A16A',
-  '54+B3B|@13@=24\\1A=34@=&13@|',
-  '57@=@|B3B1A1A1B@=B2A3A2C2A3A1@|A1C2A1B3A3A2C2A3A1A1B1@|A1A1A1B1C3C=|',
-  "57@=@`A1/1\\1A'@`A1/=|\\1\\@|@|\\1A1/|@|@=1A\\1A1/=`B|@|@|\\1A1/|@|@`B|@B|@`A1'1A1/1\\1A1/+",
-  '57@=@|@=(A|@|@=2<1@|A|@&@|A|@=|=&@=2@|A|@&@|A|@=2@|A|@|@|@=1B\\A|',
-  '57+A=+A=|A|/B,A|A=|A|A|/A/2/B,+/C\\1/B,A=|A=/C\\|A=4/B,+/C\\1/B,A|A=4/B|A|A=|A=|A|/C&',
-  '90B\\@30B\\+',
-  '88+C\\30+C\\'
-];
+const message1 = [` _______ _           _   _             _ _      __      _ _        _`,
+`|__   __| |         | | ( )           | | |    / _|    | | |      | |`,
+`   | |  | |__   __ _| |_|/ ___    __ _| | |   | |_ ___ | | | _____| |`,
+`   | |  | '_ \\ / _\` | __| / __|  / _\` | | |   |  _/ _ \\| | |/ / __| |`,
+`   | |  | | | | (_| | |_  \\__ \\ | (_| | | |_  | || (_) | |   <\\__ \\_|`,
+`   |_|  |_| |_|\\__,_|\\__| |___/  \\__,_|_|_( ) |_| \\___/|_|_|\\_\\___(_)`,
+`                                          |/`];
+
+const message2 = [` _______ _                 _                           __                                      _   _                _`,
+`|__   __| |               | |                         / _|                                    | | (_)              | |`,
+`   | |  | |__   __ _ _ __ | | __  _   _  ___  _   _  | |_ ___  _ __   _   _  ___  _   _ _ __  | |_ _ _ __ ___   ___| |`,
+`   | |  | '_ \\ / _\` | '_ \\| |/ / | | | |/ _ \\| | | | |  _/ _ \\| '__| | | | |/ _ \\| | | | '__| | __| | '_ \` _ \\ / _ \\ |`,
+`   | |  | | | | (_| | | | |   <  | |_| | (_) | |_| | | || (_) | |    | |_| | (_) | |_| | |    | |_| | | | | | |  __/_|`,
+`   |_|  |_| |_|\\__,_|_| |_|_|\\_\\  \\__, |\\___/ \\__,_| |_| \\___/|_|     \\__, |\\___/ \\__,_|_|     \\__|_|_| |_| |_|\\___(_)`,
+`                                   __/ |                               __/ |`,
+`                                  |___/                               |___/`];
+
+const secretCode = message1.concat(message2.map((a) => ' '.repeat(55).concat(a)));
+
+for (let i = 0; i < secretCode.length; i++) {
+  secretCode[i] = secretCode[i].replace(/ \| /g,'@')
+    .replace(/\(_\)/g,'&')
+    .replace(/\| /g,'=')
+    .replace(/ \|/g,'+')
+    .replace(/[\\/]/g, (m)=>(m === '\\' ? '/' : '\\'))
+    .replace(/\s+/g,(m)=>(m.length))
+    .replace(/_+/g, (m)=>('_ABCDEFGHIJKLMNOPQRSTUVWXYZ'[m.length]));
+};
+
+// const secretCode = [
+//   '1G1A11A3A13A1A6B6A1A8A',
+//   '|B3B|@7@=(1)10@|@3\\1A=2@|@4@|',
+//   '2@=@|B3B1A=|A|\\1C4B1A|@=1@|A1C@|@E=|',
+//   "2@=@`A1/1\\1A'@B=\\1B=1\\1A'@|@1@1A\\1A1/|@|\\1\\1B=|",
+//   '2@=@|@=(A|@|A2/B1/@(A|@=|A1@|=&@=2</B1/A|',
+//   '2+A=+A=|A|/B,A|/B=|C\\2/B,A|A|A(1)+A=/C\\|A|A|/A/C&',
+//   '41+\\',
+//   '56G1A17A27B38A3A16A',
+//   '54+B3B|@13@=24\\1A=34@=&13@|',
+//   '57@=@|B3B1A1A1B@=B2A3A2C2A3A1@|A1C2A1B3A3A2C2A3A1A1B1@|A1A1A1B1C3C=|',
+//   "57@=@`A1/1\\1A'@`A1/=|\\1\\@|@|\\1A1/|@|@=1A\\1A1/=`B|@|@|\\1A1/|@|@`B|@B|@`A1'1A1/1\\1A1/+",
+//   '57@=@|@=(A|@|@=2<1@|A|@&@|A|@=|=&@=2@|A|@&@|A|@=2@|A|@|@|@=1B\\A|',
+//   '57+A=+A=|A|/B,A|A=|A|A|/A/2/B,+/C\\1/B,A=|A=/C\\|A=4/B,+/C\\1/B,A|A=4/B|A|A=|A=|A|/C&',
+//   '90B\\@30B\\+',
+//   '88+C\\30+C\\'
+// ];
 
 const printOut = (lines, delay = 25, callback) => {
   let x = 0;
@@ -70,9 +99,6 @@ const groovyPrint = (lines, delay = 2) => {
       }
     }
   }, delay);
-
-
-
 }
 
 const readline = require('readline').createInterface({
@@ -103,9 +129,9 @@ console.log('\nTo decipher it, follow these steps:');
 console.log('1. Replace all occurrences of "@" with " | ".');
 
 readline.question('\nEnter the line of code to do this:\nmessage = message.replace(', (data) => {
-  if (data !== `/@/g, ' | ')`) {
+  if (data !== `/@/g, ' | ');`) {
     console.log("\n\u001b[38;5;9m\u001b[1mHmm, you could be right but here's what I was looking for:\u001b[0m")
-    console.log(`\u001b[38;5;14mmessage = message.replace(/@/g, ' | ')\u001b[0m`);
+    console.log(`\u001b[38;5;14mmessage = message.replace(/@/g, ' | ');\u001b[0m`);
   } else {
     console.log('\n\u001b[38;5;10m\u001b[1mCorrect!\u001b[0m')
   }
@@ -115,9 +141,9 @@ readline.question('\nEnter the line of code to do this:\nmessage = message.repla
   console.log('\n2. Replace all occurrences of "&" with "(_)".');
 
   readline.question('\nEnter the line of code to do this:\nmessage = message.replace(', (data) => {
-    if (data !== `/&/g, '(_)')`) {
+    if (data !== `/&/g, '(_)');`) {
       console.log("\n\u001b[38;5;9m\u001b[1mHmm, you could be right but here's what I was looking for:\u001b[0m")
-      console.log(`\u001b[38;5;14mmessage = message.replace(/&/g, '(_)')\u001b[0m`);
+      console.log(`\u001b[38;5;14mmessage = message.replace(/&/g, '(_)');\u001b[0m`);
     } else {
       console.log('\n\u001b[38;5;10m\u001b[1mCorrect!\u001b[0m')
     }
@@ -127,9 +153,9 @@ readline.question('\nEnter the line of code to do this:\nmessage = message.repla
     console.log('\n3. Replace all occurrences of "+" and "=" with " |" and "| ", respectively. (Hint: use a function)');
 
     readline.question('\nEnter the line of code to do this:\nmessage = message.replace(', (data) => {
-      if (data !== `/[+=]/g, (match) => (match === '+' ? ' |' : '| '))`) {
+      if (data !== `/[+=]/g, (match) => (match === '+' ? ' |' : '| '));`) {
         console.log("\n\u001b[38;5;9m\u001b[1mHmm, you could be right but here's what I was looking for:\u001b[0m")
-        console.log(`\u001b[38;5;14mmessage = message.replace(/[+=]/g, (match) => (match === '+' ? ' |' : '| '))\u001b[0m`);
+        console.log(`\u001b[38;5;14mmessage = message.replace(/[+=]/g, (match) => (match === '+' ? ' |' : '| '));\u001b[0m`);
       } else {
         console.log('\n\u001b[38;5;10m\u001b[1mCorrect!\u001b[0m')
       }
@@ -139,9 +165,9 @@ readline.question('\nEnter the line of code to do this:\nmessage = message.repla
       console.log('\n4. Swap all occurrences of "\\" and "/" with each other. (Remember to escape your slashes)');
 
       readline.question('\nEnter the line of code to do this:\nmessage = message.replace(', (data) => {
-        if (data !== `/[\\\\\\/]/g, (match) => (match === '\\\\' ? '\\/' : '\\\\'))`) {
+        if (data !== `/[\\\\\\/]/g, (match) => (match === '\\\\' ? '\\/' : '\\\\'));`) {
           console.log("\n\u001b[38;5;9m\u001b[1mHmm, you could be right but here's what I was looking for:\u001b[0m")
-          console.log(`\u001b[38;5;14mmessage = message.replace(/[\\\\\\/]/g, (match) => (match === '\\\\' ? '\\/' : '\\\\'))\u001b[0m`);
+          console.log(`\u001b[38;5;14mmessage = message.replace(/[\\\\\\/]/g, (match) => (match === '\\\\' ? '\\/' : '\\\\'));\u001b[0m`);
         } else {
           console.log('\n\u001b[38;5;10m\u001b[1mCorrect!\u001b[0m')
         }
@@ -150,9 +176,9 @@ readline.question('\nEnter the line of code to do this:\nmessage = message.repla
         printCode(secretCode);
         console.log('\n5. Replace all letters with a corresponding number of underscores (e.g., "A" -> "_", "B" -> "__").');
         readline.question('\nEnter the line of code to do this:\nmessage = message.replace(', (data) => {
-          if (data !== `/[A-Z]/g, (match) => ('_'.repeat('_ABCDEFG'.indexOf(match))))`) {
+          if (data !== `/[A-Z]/g, (match) => ('_'.repeat('_ABCDEFG'.indexOf(match))));`) {
             console.log("\n\u001b[38;5;9m\u001b[1mHmm, you could be right but here's what I was looking for:\u001b[0m")
-            console.log(`\u001b[38;5;14mmessage = message.replace(/[A-Z]/g, (match) => ('_'.repeat('_ABCDEFG'.indexOf(match))))\u001b[0m`);
+            console.log(`\u001b[38;5;14mmessage = message.replace(/[A-Z]/g, (match) => ('_'.repeat('_ABCDEFG'.indexOf(match))));\u001b[0m`);
           } else {
             console.log('\n\u001b[38;5;10m\u001b[1mCorrect!\u001b[0m')
           }
@@ -162,9 +188,9 @@ readline.question('\nEnter the line of code to do this:\nmessage = message.repla
           console.log('\nFinally, replace all numbers with a corresponding number of spaces (e.g., "1" -> " ", "2" -> "  ").');
 
           readline.question('\nEnter the line of code to do this:\nmessage = message.replace(', (data) => {
-            if (data !== `/\\d+/g, (match) => (' '.repeat(Number(match))))`) {
+            if (data !== `/\\d+/g, (match) => (' '.repeat(Number(match))));`) {
               console.log("\n\u001b[38;5;9m\u001b[1mHmm, you could be right but here's what I was looking for:\u001b[0m")
-              console.log(`\u001b[38;5;14mmessage = message.replace(/\\d+/g, (match) => (' '.repeat(Number(match))))\u001b[0m`);
+              console.log(`\u001b[38;5;14mmessage = message.replace(/\\d+/g, (match) => (' '.repeat(Number(match))));\u001b[0m`);
             } else {
               console.log('\n\u001b[38;5;10m\u001b[1mCorrect!\u001b[0m')
             }
